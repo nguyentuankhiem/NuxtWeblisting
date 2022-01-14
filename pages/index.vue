25cb46ffbe46f6f32f6b83b1d96ba40d8def04d6
nguyentuankhiem <tuankhiem.maicogroup@gmail.com>
2022-01-14 21:23:54 +0700

    slider for index page

--- pages/index.vue
+++ pages/index.vue
@@ -5,73 +5,12 @@
     <!--Nav-->
 
 
-    <div class="carousel relative container mx-auto" style="max-width:1600px;">
-        <div class="carousel-inner relative overflow-hidden w-full">
-            <!--Slide 1-->
-            <input class="carousel-open" type="radio" id="carousel-1" name="carousel" aria-hidden="true" hidden="" checked="checked">
-            <div class="carousel-item absolute opacity-0" style="height:50vh;">
-                <div class="block h-full w-full mx-auto flex pt-6 md:pt-0 md:items-center bg-cover bg-right" :style="{backgroundImage: `url(https://maico-hub-record.ss-hn-1.bizflycloud.vn/${post.gallery[0]})`}">
-
-                    <div class="container mx-auto">
-                        <div class="flex flex-col w-full lg:w-1/2 md:ml-16 items-center md:items-start px-6 tracking-wide">
-                            <p class="text-black text-2xl my-4">{{ post.pageInfor.title }}</p>
-                            <a class="text-xl inline-block no-underline border-b border-gray-600 leading-relaxed hover:text-black hover:border-black" href="#">view product</a>
-                        </div>
-                    </div>
-
-                </div>
-            </div>
-            <label for="carousel-3" class="prev control-1 w-10 h-10 ml-2 md:ml-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-white rounded-full bg-white hover:bg-gray-900 leading-tight text-center z-10 inset-y-0 left-0 my-auto">‹</label>
-            <label for="carousel-2" class="next control-1 w-10 h-10 mr-2 md:mr-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-white rounded-full bg-white hover:bg-gray-900 leading-tight text-center z-10 inset-y-0 right-0 my-auto">›</label>
-
-            <!--Slide 2-->
-            <input class="carousel-open" type="radio" id="carousel-2" name="carousel" aria-hidden="true" hidden="">
-            <div class="carousel-item absolute opacity-0 bg-cover bg-right" style="height:50vh;">
-                <div class="block h-full w-full mx-auto flex pt-6 md:pt-0 md:items-center bg-cover bg-right" :style="{backgroundImage: `url(https://maico-hub-record.ss-hn-1.bizflycloud.vn/${post.gallery[2]})`}">
-
-                    <div class="container mx-auto">
-                        <div class="flex flex-col w-full lg:w-1/2 md:ml-16 items-center md:items-start px-6 tracking-wide">
-                            <p class="text-black text-2xl my-4">">{{ post.pageInfor.title }}</p>
-                            <a class="text-xl inline-block no-underline border-b border-gray-600 leading-relaxed hover:text-black hover:border-black" href="#">view product</a>
-                        </div>
-                    </div>
-
-                </div>
-            </div>
-            <label for="carousel-1" class="prev control-2 w-10 h-10 ml-2 md:ml-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-white rounded-full bg-white hover:bg-gray-900  leading-tight text-center z-10 inset-y-0 left-0 my-auto">‹</label>
-            <label for="carousel-3" class="next control-2 w-10 h-10 mr-2 md:mr-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-white rounded-full bg-white hover:bg-gray-900  leading-tight text-center z-10 inset-y-0 right-0 my-auto">›</label>
-
-            <!--Slide 3-->
-            <input class="carousel-open" type="radio" id="carousel-3" name="carousel" aria-hidden="true" hidden="">
-            <div class="carousel-item absolute opacity-0" style="height:50vh;">
-                <div class="block h-full w-full mx-auto flex pt-6 md:pt-0 md:items-center bg-cover bg-bottom" :style="{backgroundImage: `url(https://maico-hub-record.ss-hn-1.bizflycloud.vn/${post.gallery[3]})`}">
-
-                    <div class="container mx-auto">
-                        <div class="flex flex-col w-full lg:w-1/2 md:ml-16 items-center md:items-start px-6 tracking-wide">
-                            <p class="text-black text-2xl my-4">">{{ post.pageInfor.title }}</p>
-                            <a class="text-xl inline-block no-underline border-b border-gray-600 leading-relaxed hover:text-black hover:border-black" href="#">view product</a>
-                        </div>
-                    </div>
-
-                </div>
-            </div>
-            <label for="carousel-2" class="prev control-3 w-10 h-10 ml-2 md:ml-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-white rounded-full bg-white hover:bg-gray-900  leading-tight text-center z-10 inset-y-0 left-0 my-auto">‹</label>
-            <label for="carousel-1" class="next control-3 w-10 h-10 mr-2 md:mr-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-white rounded-full bg-white hover:bg-gray-900  leading-tight text-center z-10 inset-y-0 right-0 my-auto">›</label>
-
-            <!-- Add additional indicators for each slide-->
-            <ol class="carousel-indicators">
-                <li class="inline-block mr-3">
-                    <label for="carousel-1" class="carousel-bullet cursor-pointer block text-4xl text-gray-400 hover:text-gray-900">•</label>
-                </li>
-                <li class="inline-block mr-3">
-                    <label for="carousel-2" class="carousel-bullet cursor-pointer block text-4xl text-gray-400 hover:text-gray-900">•</label>
-                </li>
-                <li class="inline-block mr-3">
-                    <label for="carousel-3" class="carousel-bullet cursor-pointer block text-4xl text-gray-400 hover:text-gray-900">•</label>
-                </li>
-            </ol>
-
-        </div>
+    <div>
+        <vue-glide>
+            <vue-glide-slide v-for="item in post.gallery" :key="item.id">
+                <img :src="imgUrl + item" class="h-72 w-72">
+            </vue-glide-slide>
+        </vue-glide>
     </div>
 
     <!--	 
@@ -249,6 +188,9 @@ Alternatively if you want to just have a single hero
 
 <script>
 import gql from "graphql-tag"
+import 'vue-glide-js/dist/vue-glide.css'
+import { Glide, GlideSlide } from 'vue-glide-js'
+
 export default {
   name: 'IndexPage',
   data() {
@@ -271,6 +213,7 @@ export default {
             }
         })
         {
+            id
             gallery,
             price,
             description
@@ -285,6 +228,11 @@ export default {
         post() {
             return this.posts[0]
         }
+    },
+    components: 
+    {
+        [Glide.name]: Glide,
+        [GlideSlide.name]: GlideSlide
     }
 }
 </script>
---
+++ pages/slider.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <vue-glide>
+            <vue-glide-slide v-for="i in 5" :key="i">
+                {{ i }}
+                <img src="https://vnn-imgs-f.vgcloud.vn/2021/04/12/18/can-ho-studio.jpg">
+            </vue-glide-slide>
+        </vue-glide>
+    </div>
+</template>
+
+
+
+<script>
+    import 'vue-glide-js/dist/vue-glide.css'
+    import { Glide, GlideSlide } from 'vue-glide-js'
+
+    export default 
+    {
+        components: 
+        {
+            [Glide.name]: Glide,
+            [GlideSlide.name]: GlideSlide
+        }
+    }
+</script>
